fc94fd87dcccf17a442ad99747ab3257f6178e47
Chelsea Boling <cmboling@github.com>
2021-07-19 21:13:17 -0700

    Initial commit

---
+++ Glav.CacheAdapter/Distributed/memcached/memcachedConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace Glav.CacheAdapter.Distributed.memcached
+{
+	class memcachedConstants
+	{
+		public const string CONFIG_MinimumConnectionPoolSize = "MinPoolSize";
+		public const string CONFIG_MaximumConnectionPoolSize = "MaxPoolSize";
+		public const string CONFIG_ConnectionTimeout = "ConnectionTimeout";
+	    public const string CONFIG_Protocol = "Protocol";
+	    public const string CONFIG_UserName = "UserName";
+	    public const string CONFIG_Password = "Password";
+        public const string CONFIG_DeadNodeTimeout = "DeadNodeTimeout";
+    }
+}
